fae749adea1e23fb24d0d585a552f8a226abfc9c
Etan Washbourne <ewashbourne@gmail.com>
2015-02-16 10:16:32 -0500

    Finish Static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe 'static pages' do |variable|
+	subject { page }
+
+	describe 'home page' do 
+		before { visit root_path }
+
+		it { should have_title('Home | Nutrition App') }
+		it { should have_selector('h1', text: 'Home') }
+	end
+
+	describe 'about page' do 
+		before { visit about_path }
+		
+		it { should have_title('About | Nutrition App') }
+		it { should have_selector('h1', text: 'About') }
+	end
+end
