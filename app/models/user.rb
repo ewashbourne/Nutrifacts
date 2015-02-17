fe8f1eee2d414b6d8f724921bc5250769ea6f4ad
Etan Washbourne <ewashbourne@gmail.com>
2015-02-17 17:42:43 -0500

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ActiveRecord::Base
-		before_save { email.downcase! }
+	before_save { email.downcase! }
 
-		validates :name, presence: true, length: { minimum: 3, maximum: 254 }
-		VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
-		validates :email, presence: true, length: { maximum: 254 },
-							format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+	validates :name, presence: true, length: { minimum: 3, maximum: 254 }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
+	validates :email, presence: true, length: { maximum: 254 },
+						format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 
-		has_secure_password
+	has_secure_password
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   root 'static_pages#home'
 
   get '/about', to: 'static_pages#about'
-
+  get '/new', to: 'users#new'
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
